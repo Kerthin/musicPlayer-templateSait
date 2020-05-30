111dbbbc0b85e35499adb7529d80b6889b676b6a
Kerthin <belichenko.roden@list.ru>
2020-05-30 21:37:42 +0300

    Creating a template for project development

---
+++ dist/js/main.js
@@ -0,0 +1,2 @@
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZXMiOltdLCJzb3VyY2VzQ29udGVudCI6W10sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiIifQ==
